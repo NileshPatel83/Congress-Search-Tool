37f56a9da4c3f86bdc1e48f4fd2529e7a1b0f273
NileshPatel83 <hselin_83@yahoo.com>
2023-02-02 16:42:24 +1000

    init method added to display search results when search page is displayed.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ const searchBtnAtr = 'data-index';
 //Gets DOM obects.
 let searchButtonEl = document.querySelector('#search-button');
 
+init();
+
 searchButtonEl.addEventListener('click', startSearchProcess);
 
 //Starts the process when 'Click Me' button is clicked.
@@ -27,7 +29,30 @@ function startSearchProcess(event){
 
     //If the search button in index page is clicked, launces search result page.
     if(parseInt(searchButtonEl.getAttribute(searchBtnAtr)) === 0){
+        window.location.replace(`search-results.html?search=${searchText}&format=${searchFormat}`);
 
-        window.location.replace('search-results.html');
+    //If the search button in result page is clicked, displays the result.
+    } else {
+        processResults(searchText, searchFormat);
     }
+}
+
+function init(){
+
+    //If the search button in result page is clicked, displays the result.
+    //Otherwise doesn't do anything.
+    if(parseInt(searchButtonEl.getAttribute(searchBtnAtr)) === 0){
+        return;
+    }
+
+    //Gets query parameters from url.
+    let queryParams = window.location.search.split('&');
+
+    //Gets search text from query text.
+    let searchText = queryParams[0].substring(queryParams[0].indexOf('=') + 1);
+
+    //Gets search format from query text.
+    let searchFormat = queryParams[1].substring(queryParams[1].indexOf('=') + 1);
+
+    processResults(searchText, searchFormat);
 }
